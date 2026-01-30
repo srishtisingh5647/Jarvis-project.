8bc565c2db2567556cad6fe992042abb31529c54
Srishti Singh <180443138+srishtisingh5647@users.noreply.github.com>
2026-01-30 22:31:06 +0530

    music_lib.py

---
+++ music_lib.py
@@ -0,0 +1,5 @@
+music = {
+    "stealth" : "https://youtu.be/U47Tr9BB_wE?si=ntiLqaOu4BBiP8ky",
+    "wishes"  : "https://www.youtube.com/watch?v=Gz38Yj09k3A&list=RDGz38Yj09k3A&start_radio=1",
+    "march" :"https://youtu.be/CnEqrgMlWLQ?si=_eQtyg0wSspWsm3P"
+}
